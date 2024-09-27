78691974453dab5540cdc65f7cd56cdf0653ac19
volamthienphu <phuvo@hitek.com.vn>
2024-09-27 14:23:44 +0700

    using async await mysql2

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,23 +4,36 @@ const getHomePage = (req, res) => {
   return res.render("home.ejs");
 };
 
-const postCreateUser = (req, res) => {
+const getCreatePage = (req, res) => {
+  res.render("create.ejs");
+};
+
+const postCreateUser = async (req, res) => {
   const { name, email, city } = req.body;
-  connection.query(
+
+  const [results, fields] = await connection.query(
     `INSERT INTO Users (name, email, city) VALUES (?, ?, ?)`,
-    [name, email, city],
-    function (err, results) {
-      if (err) {
-        console.error("Error inserting data: ", err);
-        return res.status(500).send("Error saving user");
-      }
-      console.log(results);
-      res.send("Save user success");
-    }
+    [name, email, city]
   );
+  console.log("🚀 ~ postCreateUser ~ results:", results);
+  res.send("Save user success");
+  //   [name, email, city],)
+  // connection.query(
+  //   `INSERT INTO Users (name, email, city) VALUES (?, ?, ?)`,
+  //   [name, email, city],
+  //   function (err, results) {
+  //     if (err) {
+  //       console.error("Error inserting data: ", err);
+  //       return res.status(500).send("Error saving user");
+  //     }
+  //     console.log(results);
+  //     res.send("Save user success");
+  //   }
+  // );
 };
 
 module.exports = {
   getHomePage,
   postCreateUser,
+  getCreatePage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -2,11 +2,14 @@ const express = require("express");
 const {
   getHomePage,
   postCreateUser,
+  getCreatePage,
 } = require("../controllers/homeController");
 const router = express.Router();
 
 router.get("/", getHomePage);
 
 router.post("/create_user", postCreateUser);
+
+router.get("/create", getCreatePage);
 
 module.exports = router;
