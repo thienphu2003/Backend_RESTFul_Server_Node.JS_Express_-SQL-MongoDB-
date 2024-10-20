bc3b904dc1061df1e46d25ca57af5d24e68d8d39
thienphuhitek <phuvo@hitek.com.vn>
2024-10-21 04:10:04 +0700

    add connection to mongodb

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,31 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  charset: "utf8mb4",
-});
+// const connection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT),
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   charset: "utf8mb4",
+// });
+
+const connection = async () => {
+  try {
+    // console.log("DB_HOST:", process.env.DB_HOST);
+    const option = {
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD,
+    };
+    await mongoose.connect(process.env.DB_HOST, option);
+    console.log(mongoose.connection.readyState);
+  } catch (error) {
+    console.log("Error connecting db >>>>", error);
+  }
+};
 
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const app = express();
 const configViewEngine = require("./config/viewEngine");
 const webRouters = require("./routes/web");
 const testDbRoute = require("./routes/test_db_connection");
+const connection = require("./config/database");
 
 const port = process.env.PORT || 3001;
 configViewEngine(app);
@@ -15,3 +16,5 @@ app.use("/test_db", testDbRoute);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+connection();
