19d0d667a7d10099f638fa4095c9d4eaac6a501d
Võ Phú <phuvo@hitek.com.vn>
2024-11-05 17:18:50 +0700

    FEAT: validate customer data

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -7,33 +7,51 @@ const {
     deleteManyCustomers,
 } = require('../services/customerService');
 const { uploadFile } = require('../services/fileService');
-
+const Joi = require('joi');
 module.exports = {
     createCustomer: async (req, res) => {
         const { name, email, address, phone, description } = req.body;
-        let imageUrl = '';
-        if (!req.files || Object.keys(req.files).length === 0) {
-            // return res.status(400).send('No files were uploaded.');
-        } else {
-            const { image } = req.files;
-            const result = await uploadFile(image);
-            imageUrl = result?.path;
-        }
+        const schema = Joi.object({
+            name: Joi.string().alphanum().min(3).max(30).required(),
 
-        const customerData = {
-            name,
-            email,
-            address,
-            phone,
-            description,
-            image: imageUrl,
-        };
+            address: Joi.string(),
 
-        const result = await createCustomer(customerData);
-        return res.status(200).json({
-            EC: 0,
-            data: result,
+            phone: Joi.string().pattern(new RegExp('^[0-9]{8,11}$')),
+
+            email: Joi.string().email(),
+
+            description: Joi.string(),
         });
+        const { error } = schema.validate(req.body);
+        if (error) {
+            res.status(400).json({
+                error: error.message,
+            });
+        } else {
+            let imageUrl = '';
+            if (!req.files || Object.keys(req.files).length === 0) {
+                // return res.status(400).send('No files were uploaded.');
+            } else {
+                const { image } = req.files;
+                const result = await uploadFile(image);
+                imageUrl = result?.path;
+            }
+
+            const customerData = {
+                name,
+                email,
+                address,
+                phone,
+                description,
+                image: imageUrl,
+            };
+
+            const result = await createCustomer(customerData);
+            return res.status(200).json({
+                EC: 0,
+                data: result,
+            });
+        }
     },
 
     createManyCustomer: async (req, res) => {
