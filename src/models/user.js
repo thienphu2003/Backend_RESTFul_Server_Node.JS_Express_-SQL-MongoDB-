94f0f42210420dc62f348beeae9a6122c0702af7
Võ Phú <phuvo@hitek.com.vn>
2024-11-05 14:59:10 +0700

    FEAT: add connection using mongodb driver

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  city: String,
+    name: String,
+    city: String,
 });
-const user = mongoose.model("user", userSchema);
+
+const user = mongoose.model('User', userSchema);
 
 module.exports = user;
--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const apiRouters = require('./routes/api');
 const testDbRoute = require('./routes/test_db_connection');
 const connection = require('./config/database');
 const fileUpload = require('express-fileupload');
-
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 3001;
 configViewEngine(app);
 // Middleware to parse URL-encoded bodies
@@ -20,7 +20,17 @@ app.use('/test_db', testDbRoute);
 
 (async () => {
     try {
+        //Using mongoose
         await connection();
+
+        //Using mongodb driver
+        // const url = process.env.MONGODB_DRIVER_URL;
+        // const client = new MongoClient(url);
+        // const dbName = process.env.DB_NAME;
+        // await client.connect();
+        // console.log('Connected successfully to server');
+        // const db = client.db(dbName);
+        // const collection = db.collection('customers');
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`);
         });
